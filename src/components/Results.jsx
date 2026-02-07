b8eafe9cd611709ee5e75e8a6d72e0c20a26eea7
francesco-mauro <francesco.mauro180@gmail.com>
2026-02-07 16:38:10 +0100

    feat: new Result Logic

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,4 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ input }) {
   console.log(input);
@@ -10,12 +10,27 @@ export default function Results({ input }) {
       <thead>
         <tr>
           <th>Anno</th>
-          <th>Investimento Iniziale</th>
-          <th>Investimento Annuale</th>
-          <th>Ritorno di Investimento Atteso</th>
-          <th>Durata</th>
+          <th>Valore Totale Investimento</th>
+          <th>Interesse (Anno)</th>
+          <th>Totale Interesse</th>
+          <th>Capitale investito</th>
         </tr>
       </thead>
+      <tbody>
+        {datiRisultato.map((yearData) => {
+          const interesseTotale = yearData.valueEndOfYear - (yearData.investimentoAnnuale * yearData.year);
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              {/* // formatter.format() è un metodo che formatta i numeri come valuta, in questo caso in dollari */}
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.investimentoAnnuale)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(yearData.investimentoAnnuale)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -8,8 +8,26 @@ export default function UserInput({ onChange, userInput }) {
             type="number"
             required
             value={userInput.investimentoIniziale}
-            onChange={(event) => {
-              onChange("investimentoIniziale", event.target.value);
+            //pippo.target.value
+            // pippo è puntamento all'oggetto che sta cambiando all'interno di imput. l'oggetto è l'evento del componente, che contrien i riferimenti all'oggetto che sto cambiando. Tale oggetto ha i vari attributi del mio tag html che potrei stare utilizzando
+            // target specifica l'elemtno del target html
+            //value è il tag html che voglio esrarre come parametro per l'onChange (react, non mia funzione)
+            //stampo anche onChange, che funziona quando sto scrivendo
+            onChange={(pippo) => {
+              onChange("investimentoIniziale", pippo.target.value);
+              console.log(pippo);
+            }}
+            //stampo anche onBlur, che funziona quando esco dal campo
+            onBlur={(pippo) => {
+              onChange("investimentoIniziale", pippo.target.value);
+              console.log(pippo);
+            }}
+            //stampo anche onKeyDown, che funziona quando premo un tasto
+            onKeyDown={(pippo) => {
+              if (pippo.key === "Enter") {
+                onChange("investimentoIniziale", pippo.target.value);
+              }
+              console.log(pippo);
             }}
           />
         </p>
